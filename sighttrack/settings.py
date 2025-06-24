1d856cb511e54a6be228b389cd9a14f40f41fe79
James Tan <jamest01230@gmail.com>
2025-06-24 08:20:34 -0400

    static files config

--- sighttrack/settings.py
+++ sighttrack/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # For app-specific static files, Django automatically finds them in app/static/ directories
 # when 'django.contrib.staticfiles' is in INSTALLED_APPS
