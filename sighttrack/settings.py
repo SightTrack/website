4c6c538ae5bd9a37323589e7d3ed7c07cb97a387
James Tan <jamest01230@gmail.com>
2025-06-24 08:17:26 -0400

    allow all hosts

--- sighttrack/settings.py
+++ sighttrack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k5*5syt$1a-)b_q!b1s^z=lg7kj@xf25tsf04984=iz4cpydj_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
