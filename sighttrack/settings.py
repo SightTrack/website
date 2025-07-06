b27eb8b3d3a8bb6889712dc2cb12a6bee20b0c96
James Tan <jamest01230@gmail.com>
2025-07-06 09:37:16 -0400

    switch to prod

--- sighttrack/settings.py
+++ sighttrack/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k5*5syt$1a-)b_q!b1s^z=lg7kj@xf25tsf04984=iz4cpydj_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['sighttrack.org', 'www.sighttrack.org', 'localhost',]
 
 
 # Application definition
